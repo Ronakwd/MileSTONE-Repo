c5daf0cde0c57e64bebfc3c72af9ffb5c11ddb23
Ronakwd <137898687+Ronakwd@users.noreply.github.com>
2023-07-24 23:41:19 +0530

    Add files via upload

---
+++ Project 4/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        Pink: '#d164fa',
+        Violet: '#5d60d6',
+        Purple: '#501aa1',
+        DarkBlue: '#1d1336',
+      },
+    },
+  },
+  plugins: [],
+}
+
